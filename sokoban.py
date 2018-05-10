e63f83947a2f54953ae26106033a1598a980dfab
AODQ <dtoadq@gmail.com>
2018-05-09 23:04:11 -0400

    2 space indentation, window doesn't resize
    Indentation is now per two spaces (as requested by IC) and the window is no longer allowed to resize.

--- sokoban.py
+++ sokoban.py
@@ -6,70 +6,75 @@ import options
 import game
 
 class Sokoban:
-    def __init__(self, w_width, w_height, caption):
-        pygame.init()
-        self.root = tkinter.Tk()
-        self.root.withdraw()
-        self.w_width = w_width
-        self.w_height = w_height
-        self.caption = caption
-        self.set_window(self.w_width, self.w_height, self.caption)
-        font = pygame.font.Font("Retroscape.ttf", 15)
-        self.menu = []
+  def __init__(self, w_width, w_height, caption):
+    pygame.init()
+    self.root = tkinter.Tk()
+    self.root.withdraw()
+    self.w_width = w_width
+    self.w_height = w_height
+    self.caption = caption
+    self.set_window(self.w_width, self.w_height, self.caption)
+    font = pygame.font.Font("Retroscape.ttf", 15)
+    self.menu = []
 
-        for i, text in enumerate(("PLAY", "EDIT", "HELP")):
-            self.menu.append(options.Option(text, self.w_width/2,
-                             (self.w_height/4)+103+(i*50), font))
-            
-        self.title = pygame.image.load("title.png").convert()
-        self.title_pos = self.title.get_rect(center=(self.w_width/2,
-                                                     self.w_height/4))
-        self.game = game.Game(self)
+    for i, text in enumerate(("PLAY", "EDIT", "HELP")):
+      self.menu.append(options.Option(text, self.w_width/2,
+               (self.w_height/4)+103+(i*50), font))
 
-    def set_window(self, w_width, w_height, caption):
-        self.window = pygame.display.set_mode((w_width, w_height))
-        pygame.display.set_caption(caption)
+    self.title = pygame.image.load("title.png").convert()
+    self.title_pos = self.title.get_rect(center=(self.w_width/2,
+                           self.w_height/4))
+    self.game = game.Game(self)
 
-    def quit(self):
-        self.root.quit()
-        pygame.quit()
-        sys.exit()
+  def set_window(self, w_width, w_height, caption):
+    # don't do this dumbass
+    w_width = 640;
+    w_height = 480;
+    self.window = pygame.display.set_mode((w_width, w_height))
+    pygame.display.set_caption(caption)
 
-    def select_file(self):
-        try:
-            return tkinter.filedialog.askopenfilename(
-                initialdir="./levels", title="Select File", filetypes=(("Sok Format",
-                ["*.sok", "*.xsb", "*.txt"]), ("All Files", "*.*")))
-        except tkinter.TclError:
-            self.quit()
+  def quit(self):
+    self.root.quit()
+    pygame.quit()
+    sys.exit()
 
-    def run(self):
-        while True:
-            self.window.fill((0, 0, 0))
-            self.window.blit(self.title, self.title_pos)
-            options.draw(self.window, self.menu)
+  def select_file(self):
+    try:
+      return tkinter.filedialog.askopenfilename(
+        initialdir="./levels", title="Select File", filetypes=(("Sok Format",
+        ["*.sok", "*.xsb", "*.txt"]), ("All Files", "*.*")))
+    except tkinter.TclError:
+      self.quit()
 
-            for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONUP:
-                    opt = options.choice(self.menu)
+  def run(self):
+    while True:
+      self.window.fill((0, 0, 0))
+      self.window.blit(self.title, self.title_pos)
+      options.draw(self.window, self.menu)
 
-                    if opt:
-                        if opt == "PLAY":
-                            file_path = self.select_file()
+      for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+          opt = options.choice(self.menu)
 
-                            if file_path:
-                                pygame.mixer.music.load("music.mp3")
-                                pygame.mixer.music.play(-1)
-                                file_path and self.game.play(file_path)
-                                pygame.mixer.music.stop()
-                            
-                        self.set_window(self.w_width, self.w_height,
-                                        self.caption)
+          if opt:
+            if opt == "PLAY":
+              file_path = self.select_file()
 
-                elif event.type == pygame.QUIT:
-                    self.quit()
+              if file_path:
+                pygame.mixer.music.load("music.mp3")
+                pygame.mixer.music.play(-1)
+                file_path and self.game.play(file_path)
+                pygame.mixer.music.stop()
 
-            pygame.display.update()
+            self.set_window(self.w_width, self.w_height,
+                    self.caption)
+
+        elif event.type == pygame.VIDEORESIZE:
+            self.set_window(640, 480, "hm");
+        elif event.type == pygame.QUIT:
+          self.quit()
+
+      pygame.display.update()
 
 if __name__ == "__main__":
    Sokoban(550, 350, "Sokoban - Menu").run()
